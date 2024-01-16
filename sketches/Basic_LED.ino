5f709fe06e6c15475e17a704f73d7f159f88749f
kaitkj <70806651+kaitkj@users.noreply.github.com>
2024-01-16 17:46:01 +0800

    Update Basic_LED.ino

--- sketches/Basic_LED.ino
+++ sketches/Basic_LED.ino
@@ -7,25 +7,22 @@ void setup() {
   Serial.begin(155200);
   pinMode(button, INPUT_PULLUP); //set button as input, with pullup (normally high)
   pinMode(LED, OUTPUT); //set LED as output
+  Serial.println();
+  Serial.println();
+  Serial.println("*** Toggle the Green button to On/Off light ***");
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   if (digitalRead(button) != 1){ //when button press, signal becomes LOW, fulfilling the condition
     buttonState = !buttonState; //flips the boolean buttonState value
-
-    if (buttonState == 1){  //when buttonState is 1, on the LED
-      digitalWrite(LED, HIGH);
-      Serial.print("LED Status:");
-      Serial.println(buttonState);
-      }
-      else{
-        digitalWrite(LED, LOW); //when buttonState is 0, off the LED
-        Serial.print("LED Status:");
-        Serial.println(buttonState);
-      }
-    while (digitalRead(button) != 1){ //hold program
-      delay(100);
+    if (buttonState == 1) {
+          digitalWrite(LED, HIGH); //when buttonState is 1, on the LED
+          Serial.println("*** Light On **"); 
+    } else{
+      digitalWrite(LED, LOW); //when buttonState is 0, off the LED
+      Serial.println("*** Light Off ***");
     }
+  delay(100);
   }
 }
