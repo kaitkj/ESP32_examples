073cd96a2c239e7081fab231b731b07be1f60c50
kaitkj <70806651+kaitkj@users.noreply.github.com>
2024-05-21 17:04:48 +0800

    Update Basic_LED.ino

--- sketches/Basic_LED.ino
+++ sketches/Basic_LED.ino
@@ -1,10 +1,16 @@
-int button = 27; //declare button to pin 27
-int LED = 14; //declare LED to pin 14
-bool buttonState = 0; //start at off
+#if CONFIG_IDF_TARGET_ESP32S3
+  int button = 4; //declare button to pin 4
+  int LED = 5; //declare LED to pin 5
+  bool buttonState = 0; //start at off
+#else
+  int button = 27; //declare button to pin 27
+  int LED = 14; //declare LED to pin 14
+  bool buttonState = 0; //start at off
+#endif
 
 void setup() {
   //put your setup code here, to run once:
-  Serial.begin(155200);
+  Serial.begin(115200);
   pinMode(button, INPUT_PULLUP); //set button as input, with pullup (normally high)
   pinMode(LED, OUTPUT); //set LED as output
   Serial.println();
